5007c0d41a208f5be7a1481c3c820b7f5253b3e0
Blake La Pierre <blakelapierre@gmail.com>
2016-03-01 17:04:41 -0800

    Initial work

---
+++ src/grammar/AngularProject.toObject.semantics.js
@@ -0,0 +1,36 @@
+import {join, first} from './util';
+
+
+export default {
+  ListOf_some (element, separator, rest) {
+    return [element.toObject()].concat(rest.toObject());
+  },
+
+  CContained (open, element, close) {
+    return element.toObject();
+  },
+
+  Application (name, modules) {
+    return join({name, modules});
+  },
+
+  Module: (name, elements) => join({name, elements}),
+
+  Requirements: (glyph, requirements) => join({requirements}),
+
+  Components: (glyph, components) => join({components}),
+
+  Component: (name, subComponents) => join({name, subComponents}),
+
+  Routes: (glyph, routes) => join({routes}),
+
+  Route: (path, colon, name) => join({path, name}),
+
+  Factories: (glyph, factories) => join({factories}),
+
+  Factory: (name) => join({name}),
+
+  name (character) {
+    return this.interval.contents;
+  }
+};
---
+++ src/tests/index.js
@@ -0,0 +1,26 @@
+require('../traceur-runtime');
+
+import util from 'util';
+
+import _ from 'lodash';
+
+import {loadGrammarWithSemantics, runFromFile} from '../ohmLoader';
+
+const {grammar, semantics} = loadGrammarWithSemantics('AngularProject', ['toObject']);
+
+const object = runFromFile('./tests/samples/2016-fantasy', grammar, semantics, 'toObject');
+
+log(util.inspect(object, false, null));
+
+// log(toPostgreSQL(orderTables(model)).join('\n'));
+
+function log(...args) {
+  console.log.apply(console, args.map(transformArg));
+
+  function transformArg(arg) {
+    switch (typeof arg) {
+      case 'object': return util.inspect(arg, {showHidden: true, depth: null});
+      default: return arg;
+    }
+  }
+}
